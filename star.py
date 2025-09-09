b2d4319cde44297aaaf6b9afcfb2ba0011de124e
Kyh0730 <rladudgks0852@gmail.com>
2025-09-09 12:29:11 +0900

    2주차과제

--- star.py
+++ star.py
@@ -2,13 +2,13 @@ class Pyramid:
     def __init__(self, height):
         self.height = height
 
-    def print_star_pyramid(self):
-        for i in range(1, self.height + 1):
+    def print_inverted_star_pyramid(self):
+        for i in range(self.height, 0, -1):
             print(" " * (self.height - i) + "*" * (2 * i - 1))
 
 
 height = int(input("피라미드 높이를 입력하세요: "))
 p = Pyramid(height)
-p.print_star_pyramid()
+p.print_inverted_star_pyramid()
 
-#commit 
+#2124014_2주차과제_수정
